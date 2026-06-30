a93529eda92b6b6c8a17ca18d5c7f89a3fcdc560
jayeshtoscano <jayeshtoscano@gmail.com>
2026-06-30 13:19:45 -0400

    Implement CacheAgent with caching methods

---
+++ agents/CacheAgent.py
@@ -0,0 +1,12 @@
+from app.services.cache_service import CacheService
+
+class CacheAgent:
+
+    def __init__(self):
+        self.cache = CacheService()
+
+    def check(self, prompt):
+        return self.cache.get(prompt)
+
+    def store(self, prompt, response):
+        self.cache.set(prompt, response)
