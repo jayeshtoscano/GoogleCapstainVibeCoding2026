035e3d6fcd37598071b0f8f1d7168412f1ca055d
jayeshtoscano <jayeshtoscano@gmail.com>
2026-07-01 15:08:49 -0400

    Integrate observability agent and scores endpoint
    Added observability agent and endpoint for scores.

--- app/api.py
+++ app/api.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter
 from orchestrator import Orchestrator
 from models.request_models import PromptRequest
+from agents.observability_agent import ObservabilityAgent
+from models.response_models import AgentTraceResponse
 
 router = APIRouter()
 
 #  global singleton Orchestrator for running multi agent system
 orchestrator = Orchestrator()
-
+observability_agent = ObservabilityAgent()
 
 @router.post("/multi-agent/gatekeeper")
 def multi_agent_pipeline(request: PromptRequest):
     return orchestrator.run(request.prompt)
+
+
+##########################################################
+# OBSERVABILITY ENDPOINT
+##########################################################
+
+@router.get("/observability/scores")
+def get_diligence_scores():
+
+    return observability_agent.project_scores()
